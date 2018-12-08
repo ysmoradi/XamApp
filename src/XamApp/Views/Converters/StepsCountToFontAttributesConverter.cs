a07cd80b65dddd946d1644932ee96182f4cc00a3
Yaser Moradi <ysmoradi@outlook.com>
2018-12-08 17:28:10 +0330

    Added converters

---
+++ src/XamApp/Views/Converters/StepsCountToFontAttributesConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamApp.Views.Converters
+{
+    public class StepsCountToFontAttributesConverter : Converter<int, FontAttributes>
+    {
+        protected override FontAttributes Convert(int value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value % 2 == 0) /* The way we detect the number is even or odd! */
+                return FontAttributes.Bold;
+            return FontAttributes.Italic;
+        }
+    }
+}
