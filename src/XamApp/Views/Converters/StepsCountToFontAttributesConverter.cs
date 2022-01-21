a31f6031b71f1cd6ccf8bd671cb53f5b9b3d9a41
ysmoradi <ysmoradi@outlook.com>
2022-01-21 10:55:04 +0330

    Updated nuget packages

--- src/XamApp/Views/Converters/StepsCountToFontAttributesConverter.cs
+++ src/XamApp/Views/Converters/StepsCountToFontAttributesConverter.cs
@@ -7,7 +7,7 @@ namespace XamApp.Views.Converters
 {
     public class StepsCountToFontAttributesConverter : ValueConverter<int, FontAttributes>
     {
-        protected override FontAttributes Convert(int value, Type targetType, object parameter, CultureInfo culture)
+        public override FontAttributes Convert(int value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value % 2 == 0) /* The way we detect the number is even or odd! */
                 return FontAttributes.Bold;
